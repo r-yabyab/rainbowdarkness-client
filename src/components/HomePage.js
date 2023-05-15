cabd9d355c35e134f149f4f63c5b7082e9b788b6
Scruffy pc <cayabyabrr@yahoo.com>
2023-05-14 21:55:23 -0700

    testing, css from prod and mobile dont reflect

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -91,7 +91,12 @@ export function HomePage ({ darkMode, graphRef }) {
         {/* <div className='overflow-hidden w-full'> */}
 
           {/* </div> */}
-
+          {/* <div className={'absolute  w-full overflow-hidden h-screen'}>
+      <div 
+          className={` ${ isLoadingComponent || destroyer ? 'hidden' : ''} ${ darkMode ? 'bg-black blur-sm -mt-2 ' : 'bg-zinc-200'} z-0 absolute min-h-[480px] w-[40000px] -left-20 overflow-hidden  shadow-sm `}
+          style={{ padding: '0 0px', margin: '0 0px' }}
+          />
+          </div> */}
       
       <div className=''>
  
