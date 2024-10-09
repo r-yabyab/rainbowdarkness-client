3be9f9022a3ceefce6a3b688361eac5721238975
Scruffy pc <cayabyabrr@yahoo.com>
2024-10-09 00:04:46 -0700

    Adding tests

---
+++ src/tests/App.test.js
@@ -0,0 +1,8 @@
+// import { render, screen } from '@testing-library/react'
+// import App from '../App'
+
+// test('test1', () =>{
+//     render(<App />)
+//     const el = screen.getByText(/test/i);
+//     expect(el).toBeInTheDocument();
+// })
